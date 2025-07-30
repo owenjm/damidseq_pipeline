f906bfb4c55c97b993d3273260680a9bb942380b
Owen Marshall <owen.marshall@utas.edu.au>
2025-07-30 15:19:17 -0700

    Updated to v1.6.1

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/damidseq_pipeline/jekyll/update/2021/06/15/welcome-to-jekyll.html",
+        "url": "/damidseq_pipeline/jekyll/update/2021/06/14/welcome-to-jekyll.html",
         "teaser": null
       }]
